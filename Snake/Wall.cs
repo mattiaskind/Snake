8be060fd000df06b83888918ed1a15952a81b3d2
Christofer Gumowski <mrdoffe@hotmail.com>
2022-02-10 12:55:04 +0100

    Lagt till grundtankar om väggar

---
+++ Snake/Wall.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Wall : GameObject
+    {
+        public Wall(GameWorld world) : base(world)
+        {
+            Appearance = '#';
+        }
+
+        public void setPosition(int x, int y)
+        {
+            Position.X = x;
+            Position.Y = y;
+        }
+
+        public override void Update()
+        {
+            // get map from Maps.cs
+            // Rita ut map
+        }
+    }
+}
